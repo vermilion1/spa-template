e299876771a8b4836ad39f833c45a16ecc11e563
Vitaliy Petrychuk <vitaliy@petrychuk.com>
2014-11-28 15:05:20 +0200

    added constants added minimum app load time (logo will be hidden at least after that time)

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -1,4 +1,12 @@
 var $ = require('../vendor/jquery');
+var constants = require('./helpers/constants');
 
-$('[data-loading]').addClass('loading--finished');
-$('[data-main]').show();
+var minLoadTime = constants.MIN_APP_LOAD_TIME;
+var diff = new Date() - window.startTime;
+var timeout = diff < minLoadTime ? minLoadTime : diff;
+var start = function () {
+    $('[data-loading]').addClass('loading--finished');
+    $('[data-main]').show();
+};
+
+setTimeout(start, timeout);
---
+++ app/js/src/helpers/constants.js
@@ -0,0 +1,5 @@
+const MIN_APP_LOAD_TIME = 500;
+
+module.exports = {
+  MIN_APP_LOAD_TIME
+};
