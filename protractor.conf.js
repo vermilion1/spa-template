f1b71212481b4b1c248851bc83327323d5d66211
Vitaliy Petrychuk <vitaliy@petrychuk.com>
2014-12-30 18:17:24 +0200

    made prtractor windows compatible

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,6 +3,7 @@ var configFile = require('./config');
 module.exports.config = {
 
   seleniumServerJar: configFile.protractor.seleniumServerJar,
+  chromeDriver: configFile.protractor.chromeDriver,
   specs: configFile.protractor.specs,
 
   capabilities: {
